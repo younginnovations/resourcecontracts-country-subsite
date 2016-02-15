0b6879ad955bf1991295dcf6b1b3d3f406e29d6c
bijaya.kuikel <bijaya.kuikel@yipl.com.np>
2016-02-15 16:09:30 +0545

    Footer Changed in country sub site

--- resources/lang/en/footer.php
+++ resources/lang/en/footer.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    'message' => 'This site provides summaries of contracts and their terms to facilitate understanding of important provisions in the documents. These summaries are not interpretations of the documents. Neither the summaries nor the full contracts are complete accounts of all legal obligations related to the projects in question. This site also includes document text that was created automatically; such text may contain errors and differences from the original PDF files. No warranty is made to the accuracy of any content on this website.',
-    'about' => 'About',
-    'glossary' => 'Glossary',
-    'publish_contracts' => 'Publish Contracts',
-    'contact' => 'Contact',
+    'message'            => 'This site provides summaries of contracts and their terms to facilitate understanding of important rovisions in the documents. These summaries are not interpretations of the documents. Neither the summaries nor the full contracts are complete accounts of all legal obligations related to the projects in question. This site also includes document text that was created automatically; such text may contain errors and differences from the original PDF files. No warranty is made to the accuracy of any content on this website.',
+    'further_information' => 'For more information please visit the National Minerals Agency website at <a target="_blank" href="http://www.nma.gov.sl">www.nma.gov.sl</a> or the Ministry of Mines and Mineral Resources website at <a  target="_blank" href="http://www.slminerals.org">www.slminerals.org</a>.',
+    'about'              => 'About',
+    'glossary'           => 'Glossary',
+    'publish_contracts'  => 'Publish Contracts',
+    'contact'            => 'Contact',
 
 ];
--- resources/lang/fr/footer.php
+++ resources/lang/fr/footer.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    'message' => 'Something in French. Something in FrenchSomething in French Something in French Something in French   Something in French Something in French Something in French  Something in French Something in French Something in FrenchSomething in FrenchSomething in French something in french  ',
-    'about' => 'Environ',
-    'glossary' => 'Glossaire',
-    'publish_contracts' => 'Publier Contrats',
-    'contact' => 'Contacter'
+    'message'             => 'Something in French. Something in FrenchSomething in French Something in French Something in French   Something in French Something in French Something in French  Something in French Something in French Something in FrenchSomething in FrenchSomething in French something in french  ',
+    'further_information' => 'Something in french',
+    'about'               => 'Environ',
+    'glossary'            => 'Glossaire',
+    'publish_contracts'   => 'Publier Contrats',
+    'contact'             => 'Contacter'
 
 ];
